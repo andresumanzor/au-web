c81c05490f7fe3d3219c7bf3814667ae4b045e38
Andrés Umanzor <andresumanzor@MacBook-Pro.local>
2018-01-14 20:40:09 -0600

    design updated and new features added

---
+++ index.js
@@ -0,0 +1,7 @@
+document.querySelector('#button-github').addEventListener('click', function() {
+    window.open("https://github.com/andresumanzor", "");
+});
+
+document.querySelector('#button-email').addEventListener('click', function() {
+    window.location.href = "mailto:andres.umanzor@hotmail.com?Subject=Website%20Contact";
+});
