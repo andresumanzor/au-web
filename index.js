7a3b8fb7d54df23761f985dcf4440b7dac36c9ba
Andres Umanzor <andres.umanzor@hotmail.com>
2019-01-11 10:12:34 -0600

    js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-document.querySelector('#button-github').addEventListener('click', () => {
-    window.open("https://github.com/andresumanzor", "");
-});
+$( document ).ready(function() {
+    document.querySelector('#button-github').addEventListener('click', () => {
+	    window.open("https://github.com/", "");
+	});
 
-document.querySelector('#button-email').addEventListener('click', () => {
-    window.location.href = "mailto:andres.umanzor@hotmail.com?Subject=Website%20Contact";
+	document.querySelector('#button-email').addEventListener('click', () => {
+	    window.location.href = "mailto:andres.umanzor@hotmail.com?Subject=Website%20Contact";
+	});
 });
