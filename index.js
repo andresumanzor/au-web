64209facd7b757477fb33281b30a48ba3f23fe19
Andres Umanzor <andres.umanzor@hotmail.com>
2019-04-01 16:02:32 -0600

    commenting unused file

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-$( document ).ready(function() {
-    document.getElementById('button-github').click((event) => {
-    	event.preventDefault();
-	    window.open("https://github.com/", "");
-	});
+// $( document ).ready(function() {
+//     document.getElementById('button-github').click((event) => {
+//     	event.preventDefault();
+// 	    window.open("https://github.com/", "");
+// 	});
 
-	document.getElementById('button-email').click((event) => {
-		event.preventDefault();
-	    window.location.href = "mailto:andres.umanzor@hotmail.com?Subject=Website%20Contact";
-	});
-});
+// 	document.getElementById('button-email').click((event) => {
+// 		event.preventDefault();
+// 	    window.location.href = "mailto:andres.umanzor@hotmail.com?Subject=Website%20Contact";
+// 	});
+// });
